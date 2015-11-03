671effe62f05286e85450e346937a8d8e1e78111
vietduu <hungvietduu@gmail.com>
2015-11-03 18:18:06 +0700

    add bob pet
    # Conflicts:
    #	.zfproject.xml
    #	application/configs/application.ini
    #	application/modules/bob/controllers/PetController.php
    #	application/modules/bob/views/scripts/pet/index.phtml
    #	application/views/scripts/index/index.phtml
    #	tests/application/modules/bob/controllers/PetControllerTest.php

--- tests/application/modules/bob/controllers/PetControllerTest.php
+++ tests/application/modules/bob/controllers/PetControllerTest.php
@@ -9,6 +9,31 @@ class Bob_PetControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         parent::setUp();
     }
 
+<<<<<<< HEAD
+    public function testAddAction()
+    {
+        $params = array('action' => 'add', 'controller' => 'Pet', 'module' => 'bob');
+        $urlParams = $this->urlizeOptions($params);
+        $url = $this->url($urlParams);
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($urlParams['module']);
+        $this->assertController($urlParams['controller']);
+        $this->assertAction($urlParams['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
 
 }
 
+
+
+=======
+
+}
+
+>>>>>>> 1d0a7bc... add bob pet
